6ed4d71c70132ebda1ec725275906fdca53c2dbc
sheinhtet715 <sheinhtet715@gmail.com>
2025-08-13 21:16:56 +0630

    add main menu by Aye Su Mon, Marina Aye Thidar Tun

---
+++ Main_menu/menu/main_intro.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
+
+func  _ready():
+	video_stream_player.play()
+	video_stream_player.finished.connect(on_finished)
+	
+func on_finished():
+	get_tree().change_scene_to_file("res://Main_menu/menu/Main_Menu.tscn")
