48a192b6002961679d1f6528cf8146ed2d6353f6
PeterJohn1234 <bhonemyintchain@gmail.com>
2025-08-19 00:21:15 +0700

    This is the first sample House

---
+++ Assets/Chapters/chapter_2_1.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var spawn_name = GameState.spawn_point_name
+	var spawn_position = $SpawnPoints.get_node(spawn_name).global_position
+	$CharacterBody2D.global_position = spawn_position
